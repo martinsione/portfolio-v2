792aed5d3e04eef1300de9be5624348f509cbe92
Martin Sione <martinsione03@gmail.com>
2021-08-10 13:20:14 -0300

    Add layout and navbar components

---
+++ components/Layout.tsx
@@ -0,0 +1,50 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import Navbar from "@/components/Navbar";
+
+export default function Layout({
+  title,
+  description,
+  children,
+}: {
+  title?: string;
+  description?: string;
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+
+  const meta = {
+    title: title ? `${title} | Martin Sione` : "Martin Sione",
+    description: description || "Front-end developer, lifelong learner.",
+    image: "https://martinsione.com/static/images/banner.png",
+    type: "website",
+  };
+  return (
+    <>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta name="description" content={meta.description} />
+        <meta
+          property="og:url"
+          content={`https://martinsione.com${router.asPath}`}
+        />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Martin Sione" />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+        <link
+          rel="canonical"
+          href={`https://martinsione.com${router.asPath}`}
+        />
+      </Head>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { FaSun, FaMoon } from "react-icons/fa";
+
+export default function Navbar() {
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
+  useEffect(() => setMounted(true), []);
+
+  return (
+    <nav className="sticky top-0 flex justify-between items-center max-w-4xl w-full p-8 my-0 md:my-8 mx-auto bg-white dark:bg-black text-lg">
+      <a href="#skip" className="sr-only focus:not-sr-only">
+        Skip to content
+      </a>
+      <button
+        aria-label="Toggle Dark Mode"
+        type="button"
+        className="flex items-center justify-center rounded bg-gray-200 dark:bg-gray-800 h-10 w-10"
+        onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      >
+        {mounted && (resolvedTheme === "dark" ? <FaSun /> : <FaMoon />)}
+      </button>
+      <div>
+        <Link href="/about">
+          <a className="pl-4 text-gray-900 dark:text-gray-100">About</a>
+        </Link>
+        <Link href="/">
+          <a className="pl-4 text-gray-900 dark:text-gray-100">Home</a>
+        </Link>
+      </div>
+    </nav>
+  );
+}
